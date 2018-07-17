5f25c74f6eaa57219247cfec7b10e97aa1bf5d67
Austin Benitez <austin.i.davis94@.com>
2018-07-17 16:16:24 -0600

    Moved javascript code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const interact = require('interactjs');
+
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,58 +8,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/test', defineInteract(interact))
+  .get('/test', (req, res) => res.render('pages/mainTable'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function renderMain(ineract, res){
   defineInteract(interact)
   res.render('pages/mainTable')
 }
-
-function defineInteract(interact)
-  // target elements with the "draggable" class
-  interact('.draggable')
-    .draggable({
-      // enable inertial throwing
-      inertia: true,
-      // keep the element within the area of it's parent
-      restrict: {
-        restriction: "parent",
-        endOnly: true,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-      },
-      // enable autoScroll
-      autoScroll: true,
-
-      // call this function on every dragmove event
-      onmove: dragMoveListener,
-      // call this function on every dragend event
-      onend: function (event) {
-        var textEl = event.target.querySelector('p');
-
-        textEl && (textEl.textContent =
-          'moved a distance of '
-          + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                       Math.pow(event.pageY - event.y0, 2) | 0))
-              .toFixed(2) + 'px');
-      }
-    });
-
-    function dragMoveListener (event) {
-      var target = event.target,
-          // keep the dragged position in the data-x/data-y attributes
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-      // translate the element
-      target.style.webkitTransform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
-
-      // update the posiion attributes
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    }
-
-    // this is used later in the resizing and gesture demos
-    window.dragMoveListener = dragMoveListener;
