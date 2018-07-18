d5c7bccec23e38ea8db88dde3216575479e30ff4
Austin Benitez <austin.i.davis94@.com>
2018-07-17 19:49:18 -0600

    moved the top bit to the top of the listener

--- public/controllers/interactController.js
+++ public/controllers/interactController.js
@@ -30,6 +30,8 @@
     });
 
     function dragMoveListener (event) {
+      //Put on top:
+      event.target.parentNode.appendChild(event.target);
       var target = event.target,
           // keep the dragged position in the data-x/data-y attributes
           x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -43,8 +45,7 @@
       // update the posiion attributes
       target.setAttribute('data-x', x);
       target.setAttribute('data-y', y);
-      //Put on top:
-      target.parentNode.appendChild(target);
+
     }
 
     // this is used later in the resizing and gesture demos
