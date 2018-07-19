8eaac7843e5c51115aa84234efa95fa1dd6b2363
Austin Benitez <austin.i.davis94@.com>
2018-07-19 14:38:51 -0600

    aDDED  ajax functionality and mtgapi functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mtg = require('mtgsdk')
 
 const PORT = process.env.PORT || 5000
 
@@ -9,9 +10,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/test', (req, res) => res.render('pages/mainTable'))
+  .get('/findCard')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function renderMain(ineract, res){
   defineInteract(interact)
   res.render('pages/mainTable')
+}
+
+function findCard(req, res)
+{
+  var result = mtg.card.find(3)(result, function(err, res){
+    if (err){
+      console.log("error");
+      res.status(500).json({success: false});
+    }
+    else{
+      res.JSON(result);
+    }
+  });
 }
--- public/controllers/MTGinteract.js
+++ public/controllers/MTGinteract.js
@@ -1,8 +1,23 @@
-const mtg = require('mtgsdk');
+
+function callAjax(url, callback){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(xhttp.readyState == 4 && xhttp.status == 200){
+      callback(xhttp.responseText);
+    }
+  }
+}
+
 
 function findCard(){
+  callAjax("getCard", callback)
   mtg.card.find(3)
   .then(result => {
     console.log(result.card);
   });
+}
+
+function returnCard(result)
+{
+  console.log(result)
 }
